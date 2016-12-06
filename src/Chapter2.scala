25860f68aac3f1fd2e8fbb217b2caa57637bf4a5
Franz Amador <Franz Amador>
2016-12-05 19:48:41 -0800

    Initial commit

---
+++ src/Chapter2.scala
@@ -0,0 +1,17 @@
+object Chapter2 {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(times: Int, prevprev: Int, prev: Int): Int = {
+      val next = prevprev + prev
+      if (times <= 1) next
+      else loop(times - 1, prev, next)
+    }
+
+    if (n <= 1) n
+    else loop(n-1, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+
+  }
+}
